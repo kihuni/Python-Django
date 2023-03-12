32b13f2f79cfef65349444cebad9d01c3edecc60
stephen kihuni <stephenkihuni55@gmail.com>
2023-03-12 07:55:56 +0300

    added while and for loop

---
+++ condition/for_loops.py
@@ -0,0 +1,5 @@
+# used for looping over sequence
+nums = [1,4,5,6]
+
+for num in nums:
+    print(num)
---
+++ condition/while.py
@@ -0,0 +1,6 @@
+# While loop in python
+
+i = 1
+while i < 6:
+    print(i, 'print until i is less than 6')
+    i += 1 # increment i until i is == 6
