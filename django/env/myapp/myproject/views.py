912b83c8777240393478e49e25795c2d49c156f7
stephen kihuni <stephenkihuni55@gmail.com>
2023-03-17 21:11:43 +0300

    added django models

--- django/env/myapp/myproject/views.py
+++ django/env/myapp/myproject/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 1
+    feature1.name = 'kihuni'
+    feature1.details = 'Never give up my friend'
+
+    feature2 = Feature()
+    feature2.id = 2
+    feature2.name = 'stephen'
+    feature2.details = 'Dont worry to much bro'
+
+    features = [feature1, feature2]
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
-    texts = request.GET['texts']
+    texts = request.POST['texts']
     amount_of_words = len(texts.split())
     return render(request, 'counter.html', {'amount':amount_of_words})
 
